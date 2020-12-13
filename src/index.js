06ece5301f8de00c5d0a72023c4938da4a993228
Marek Gojdź <marek.gojdz@questy.pl>
2020-12-13 15:41:06 +0100

    nesting components in comments section

--- src/index.js
+++ src/index.js
@@ -6,7 +6,24 @@ import CommentDetail from './CommentDetail';
 const App = () => {
 	return (
 		<div className="ui container comments">
-			<CommentDetail />
+			<CommentDetail 
+			author="Sam" 
+			timeAgo="Today at 4:45PM" 
+			comment="Best experience ever"
+			avatar={faker.image.image()}
+			/>
+			<CommentDetail 
+			author="Alex" 
+			timeAgo="Today at 5:42PM" 
+			comment="Amazing!"
+			avatar={faker.image.image()}
+			/>
+			<CommentDetail 
+			author="Jane" 
+			timeAgo="Yesterday at 2:20PM" 
+			comment="Hey! What do you think about the show?"
+			avatar={faker.image.image()}
+			/>
 		</div>
 		);
 };
