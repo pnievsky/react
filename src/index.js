e91ed3a38ddf62e9a634f85fe8d4202216a0c08c
pnievsky <pniewski.b@o2.pl>
2020-12-16 15:34:59 +0100

    ApprovalCard

--- src/index.js
+++ src/index.js
@@ -2,30 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
 	return (
 		<div className="ui container comments">
-			<CommentDetail 
-			author="Sam" 
-			timeAgo="Today at 4:45PM" 
-			comment="Best experience ever"
-			avatar={faker.image.image()}
-			/>
-			<CommentDetail 
-			author="Alex" 
-			timeAgo="Today at 5:42PM" 
-			comment="Amazing!"
-			avatar={faker.image.image()}
-			/>
-			<CommentDetail 
-			author="Jane" 
-			timeAgo="Yesterday at 2:20PM" 
-			comment="Hey! What do you think about the show?"
-			avatar={faker.image.image()}
-			/>
+			<ApprovalCard>
+				<CommentDetail 
+				author="Sam" 
+				timeAgo="Today at 4:45PM" 
+				comment="Best experience ever"
+				avatar={faker.image.image()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail 
+				author="Alex" 
+				timeAgo="Today at 5:42PM" 
+				comment="Amazing!"
+				avatar={faker.image.image()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail 
+				author="Jane" 
+				timeAgo="Yesterday at 2:20PM" 
+				comment="Hey! What do you think about the show?"
+				avatar={faker.image.image()}
+				/>
+			</ApprovalCard>
 		</div>
 		);
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root')); 
